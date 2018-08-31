29646dc32554b53737fbc57bd088cfc4b8c33007
Lubor Martinský <lubor.martinsky@hotmail.com>
2018-08-31 14:40:10 +0200

    chodi

--- 11.01_Events/Program.cs
+++ 11.01_Events/Program.cs
@@ -8,8 +8,49 @@ namespace _11._01_Events
 {
     class Program
     {
+ 
         static void Main(string[] args)
         {
+            Bulb bulb = new Bulb();
+            bulb.BulbOn += Bulb_BulbOn;
+
+            bulb.Switch();
+            Console.WriteLine(bulb.state);
+            bulb.Switch();
+            Console.WriteLine(bulb.state);
+            bulb.Switch();
+            Console.WriteLine(bulb.state);
+            bulb.Switch();
+            Console.WriteLine(bulb.state);
+            bulb.Switch();
+            Console.WriteLine(bulb.state);
+
         }
+
+        static void Bulb_BulbOn(Bulb bulb)
+        {
+            Console.WriteLine("Bulb is on");
+        }
+
+        // Classes
+        public delegate void BulbOnDelegate(Bulb bulb);
+        public class Bulb
+        {
+            public bool state = false;
+            public event BulbOnDelegate BulbOn;
+            
+
+            public void Switch()
+            {
+                this.state = !this.state;
+                if ( this.state && this.BulbOn != null)
+                {
+                    this.BulbOn(this);
+                }
+            }
+        }
+
+       
+
     }
 }
